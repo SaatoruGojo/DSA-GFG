c391ec5a6e30084f77414883dfc3d953c4f1d227
harshanand23561 <72392089+harshanand23561@users.noreply.github.com>
2022-10-04 10:38:13 +0530

    4 oct 2022

--- set/repeative_element.cpp
+++ set/repeative_element.cpp
@@ -7,17 +7,19 @@ void solve(int *a, int n)
     unordered_set<int> s;
     for (int i = 0; i < n; i++)
     {
-        if (s.find(a[i]) == s.end())
+        if (s.find(a[i]) != s.end())
+        {
             cout << a[i] << ' ';
-        s.insert(a[i]);
+            s.erase(s.find(a[i]));
+        }
+        else
+            s.insert(a[i]);
     }
 }
-
 int main()
 {
     system("CLS");
-    int a[] = {2, 3, 4, 5, 3, 6, 7, 2};
+    int a[] = {2, 3, 4, 5, 3, 6, 2, 2};
     solve(a, 8);
-
     return 0;
 }
